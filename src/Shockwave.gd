718d048e5dae7b6754dd5fa778d415a11b6886b0
Antti Haavikko <antti.haavikko@focus.fi>
2020-09-13 18:15:52 +0300

    Try out bloom

--- src/Shockwave.gd
+++ src/Shockwave.gd
@@ -16,9 +16,9 @@ func boom(pos: Vector2):
 	var offset = (2.5 * res + pos - cam.position) / 5.0
 	var uv := Vector2(offset.x / res.x, 1 - offset.y / res.y)
 	material.set_shader_param("origin", uv)
-	tweener.interpolate_property(self, "size", 0, 0.4, 1.3, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
-	tweener.interpolate_property(self, "thickness", 0.2, 0, 1.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	tweener.interpolate_property(self, "amount", 0.02, 0, 1.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tweener.interpolate_property(self, "size", 0, 0.6, 1.5, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	tweener.interpolate_property(self, "thickness", 0.2, 0, 1.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tweener.interpolate_property(self, "amount", 0.02, 0, 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	tweener.start()
 
 func _process(delta):
